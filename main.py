411f632a829459cd12c37e318c6e091903f05ae5
CHJ <102904091+Chenjie1002@users.noreply.github.com>
2025-01-10 23:03:55 +0800

    Update main.py
    add telegram bot token

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "7911169476:AAFZ2_RJu2Fi3zwYVYQTtTlCGbsj35b89LU"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
